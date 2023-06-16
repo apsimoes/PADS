455c6314e38205b033e23093c7c52a53c301b11a
Suelane Garcia Fontes <suelanegarcia@gmail.com>
2023-06-16 17:04:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-flask --host=0.0.0.0 --port=5000 run app
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
